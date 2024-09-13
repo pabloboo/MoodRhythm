871196682c4be47051b080fc4305da5907251c56
Pablo Boo <pabloboocastrofeito@gmail.com>
2024-09-13 16:29:28 +0200

    Changed app name, icon and color palette

--- app/src/main/java/com/moodrhythm/ui/theme/Theme.kt
+++ app/src/main/java/com/moodrhythm/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.moodrhythm.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,28 +8,33 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color(0xFF119ae8), // Key buttons and highlighted elements
+    secondary = Color(0xFFfea203), // Secondary buttons or elements that need attention
+    tertiary = Color(0xFFfe1f4e), // Warning icons, critical actions, or error messages
+    background = Color(0xFF121212), // Main background of the app
+    surface = Color(0xFF1E1E1E), // Cards, containers, and surfaces
+    onPrimary = Color.White, // Text on primary elements like buttons or highlighted text
+    onSecondary = Color(0xFF121212), // Text for secondary buttons or secondary text elements
+    onTertiary = Color.White, // Text on elements with the red tertiary color
+    onBackground = Color(0xFFf5f4f0), // General text on the dark background
+    onSurface = Color(0xFFf5f4f0) // Text on surfaces (cards, containers)
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
+    primary = Color(0xFF0f8de7),
+    secondary = Color(0xFFfba105),
+    tertiary = Color(0xFFf32848),
     background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    surface = Color(0xFFF5F5F5),
     onPrimary = Color.White,
-    onSecondary = Color.White,
+    onSecondary = Color(0xFF1C1B1F),
     onTertiary = Color.White,
     onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    onSurface = Color(0xFF1C1B1F)
 )
 
 @Composable
